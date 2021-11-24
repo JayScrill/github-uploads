5c0d1ed22316529a188bc2bf0ccd665ee9f5e92a
Joel <ProtonSec22@protonmail.com>
2021-11-24 00:41:04 -0600

    Js Assignment Update

---
+++ Bootstrap Project/JsAssignment.js
@@ -0,0 +1,56 @@
+function myFunction() {
+    var x = document.getElementById('box1');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById('box2');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById('box3');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function myFunc() {
+   if (document.getElementById('box1').style.backgroundColor == "purple") {
+       document.getElementById('box1').style.backgroundColor = "white";
+   } else {
+       document.getElementById('box1').style.backgroundColor = "purple";
+   }
+
+}
+
+function myFunc2() {
+    if (document.getElementById('box2').style.backgroundColor == "pink") {
+        document.getElementById('box2').style.backgroundColor = "gray";
+    } else {
+        document.getElementById('box2').style.backgroundColor = "pink";
+    }
+}
+
+function myFunc3() {
+    if (document.getElementById('box3').style.backgroundColor == "orange") {
+        document.getElementById('box3').style.backgroundColor = "blue";
+    } else {
+        document.getElementById('box3').style.backgroundColor = "orange";
+    }
+}
+
+/*function changeAll() {
+    if (document.getElementById('m'))
+}*/
